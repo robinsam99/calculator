d2143da02b46eeaf3d14b761c887354163d9f49d
robin <robin@robu.com>
2019-08-04 20:09:30 +0530

    Latest

--- calc.js
+++ calc.js
@@ -1,34 +1,11 @@
-function add_test(id)
+function common(id)
 {   var x;
     x = document.getElementById('input').value;
     document.getElementById('input').value = "";
     document.getElementById('test').innerHTML += x + id; 
     
 }
-function sub(id)
-{   var x;
-    
-    x = document.getElementById('input').value;
-    document.getElementById('input').value = "";
-    document.getElementById('test').innerHTML += x + id; 
-    
-}
-function prod(id)
-{   var x;
-    
-    x = document.getElementById('input').value;
-    document.getElementById('input').value = "";
-    document.getElementById('test').innerHTML += x + id; 
-    
-}
-function divide(id)
-{   var x;
-    
-    x = document.getElementById('input').value;
-    document.getElementById('input').value = "";
-    document.getElementById('test').innerHTML += x + id; 
-    
-}
+
 function eq_final()
 {   
     
